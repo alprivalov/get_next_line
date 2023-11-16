dcc93afccee688b8c492e076b7a7c4ccc6be8404
alprival <alexprivalov@outlook.fr>
2023-11-16 20:59:26 +0100

    finished project

---
+++ get_next_line.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alprival <alprival@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 12:41:40 by alprival          #+#    #+#             */
+/*   Updated: 2022/10/02 15:21:42 by alprival         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int	ft_lenword(char *tmp)
+{
+	int	i;
+
+	i = 0;
+	while (tmp[i])
+	{
+		if (tmp[i] == '\n')
+			return (i + 1);
+		i++;
+	}
+	return (i);
+}
+
+static char	*ft_join(int fd, char **save, char *buf, char *tmp)
+{
+	int		i;
+	char	*line;
+	int		len;
+
+	i = 1;
+	tmp = ft_strjoin(tmp, save[fd]);
+	free(save[fd]);
+	while (!ft_strchr(tmp, '\n') && i)
+	{
+		i = read(fd, buf, BUFFER_SIZE);
+		buf[i] = '\0';
+		tmp = ft_strjoin(tmp, buf);
+	}
+	len = ft_lenword(tmp);
+	line = ft_substr(tmp, 0, len);
+	save[fd] = ft_substr(tmp, len, ft_strlen(tmp) - len);
+	free(buf);
+	free(tmp);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*save[1024];
+	char		*line;
+	char		*buf;
+	char		*tmp;
+
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buf || fd < 0 || read(fd, buf, 0) == -1 || BUFFER_SIZE < 1)
+		return (free(buf), NULL);
+	buf[BUFFER_SIZE] = '\0';
+	tmp = ft_calloc(1, sizeof(char));
+	if (!save[fd])
+		save[fd] = ft_calloc(1, sizeof(char));
+	line = ft_join(fd, save, buf, tmp);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alprival <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 12:51:29 by alprival          #+#    #+#             */
+/*   Updated: 2022/05/10 12:51:30 by alprival         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif
